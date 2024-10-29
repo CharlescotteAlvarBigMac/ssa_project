87d99f45c180535b6299fdfa97bf2151903791f1
Charles <charles@gmail.com>
2024-10-29 16:27:06 +1100

    User Registration

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 urlpatterns = [
     path("", views.user, name="user"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 ]
+
