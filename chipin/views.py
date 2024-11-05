0289ae8b6a7c38a31c1fdbdd2d6631dbe0e3bfd2
“FirstName <charlescotte@Baby-Jacob-Cute.local>
2024-11-05 15:23:00 +1100

    Group Join Requests

--- chipin/views.py
+++ chipin/views.py
@@ -14,13 +14,49 @@ def home(request):
     return render(request, "chipin/home.html")
 
 @login_required
+def accept_invite(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    user_id = request.GET.get('user_id')
+    if user_id:
+        invited_user = get_object_or_404(User, id=user_id)
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+        elif invited_user in group.invited_users.all():
+            group.members.add(invited_user)
+            group.invited_users.remove(invited_user)  # Remove from invited list
+            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+        else:
+            messages.error(request, "You are not invited to join this group.")
+    else:
+        messages.error(request, "Invalid invitation link.")  
+    return redirect('chipin:group_detail', group_id=group.id)
+
+@login_required
+def invite_users(request, group_id):
+    group = get_object_or_404(Group, id=group_id)
+    users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        invited_user = get_object_or_404(User, id=user_id)      
+        if invited_user in group.invited_users.all():
+            messages.info(request, f'{invited_user.username} has already been invited.')
+        else:
+            group.invited_users.add(invited_user)
+            messages.success(request, f'Invitation sent to {invited_user.username}.')
+        return redirect('chipin:group_detail', group_id=group.id)  
+    return render(request, 'chipin/invite_users.html', {
+        'group': group,
+        'users_not_in_group': users_not_in_group
+    })
+
+@login_required
 def create_group(request):
     if request.method == 'POST':
         form = GroupCreationForm(request.POST, user=request.user)
         if form.is_valid():
             group = form.save()
             messages.success(request, f'Group "{group.name}" created successfully!')
-            return redirect('group_detail', group_id=group.id)
+            return redirect('chipin:group_detail', group_id=group.id)
     else:
         form = GroupCreationForm(user=request.user)
     return render(request, 'chipin/create_group.html', {'form': form})
@@ -41,16 +77,17 @@ def delete_group(request, group_id):
     return redirect('chipin:home')
 
 @login_required
-def invite_users(request, group_id):
+def accept_invite(request, group_id):
     group = get_object_or_404(Group, id=group_id)
-    users_not_in_group = User.objects.exclude(id__in=group.members.values_list('id', flat=True))
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        send_invitation_email(group, email, request)
-        messages.success(request, f'Invitation sent to {email}')
-        return redirect('chipin:group_detail', group_id=group.id)
-    return render(request, 'chipin/invite_users.html', {'group': group, 'users_not_in_group': users_not_in_group})
-
-
-
-
+    encoded_email = request.GET.get('email')
+    if encoded_email:
+        invited_email = urllib.parse.unquote(encoded_email)
+        invited_user = get_object_or_404(User, email=invited_email)
+        if invited_user in group.members.all():
+            messages.info(request, f'{invited_user.username} is already a member of the group "{group.name}".')
+        else:
+            group.members.add(invited_user)
+            messages.success(request, f'{invited_user.username} has successfully joined the group "{group.name}".')
+    else:
+        messages.error(request, "Invalid invitation link.")
+    return redirect('chipin:group_detail', group_id=group.id)
